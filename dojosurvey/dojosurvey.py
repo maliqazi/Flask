bcd02318b1c6fe9d780ce70f247bb68890b42140
Ali Qazi <qazi.alimohammad@gmail.com>
2017-12-02 21:29:51 -0800

    Modified to use validations

--- dojosurvey/dojosurvey.py
+++ dojosurvey/dojosurvey.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect,session, flash
 app = Flask(__name__)
+app.secret_key = 'KeepItSecretKeepItSafe'
 
 @app.route('/')
 def index():
@@ -11,11 +12,17 @@ def create_user():
    location = request.form['location']
    language = request.form['language']
    comments = request.form['comments']
-   return render_template("result.html",name=name,location=location,language=language,comments=comments)
+   if len(name) < 1 or len(comments) < 1:
+       flash("Name and comments cannot be empty!")
+   elif len(comments) > 120:
+       flash("Comments should be 120 characters or less")
+   else:
+       return render_template("result.html",name=name,location=location,language=language,comments=comments)
+   return redirect('/')
 
 @app.route('/redir')
 def redir():
     print "Redirecting to index.html"
     return redirect('/')
 
-app.run(debug=True) 
+app.run(debug=True)
